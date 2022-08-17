cddbe4edb4b1c225004870efa4f6365bd303265b
bengris32 <bengris32@protonmail.ch>
2022-08-17 18:53:04 +0100

    X6833B: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4cddc98e91724c86e7761fee41d4f9f5554c7203
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
