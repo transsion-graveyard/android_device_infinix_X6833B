2e42675e544ef4211a11b409c478be6051e7f084
bengris32 <bengris32@protonmail.ch>
2022-10-07 17:42:41 +0100

    CK7n: Build lineage libperfmgr
    Change-Id: I8282e47e543d4bc466a4bc464e1c5f61741d3a64

--- device.mk
+++ device.mk
@@ -289,11 +289,15 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-V2-ndk.vendor \
-    android.hardware.power@1.3.vendor 
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_PACKAGES += \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub
+    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
+    vendor.mediatek.hardware.mtkpower@1.0.vendor \
+    vendor.mediatek.hardware.mtkpower@1.1.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3.vendor
 
 # Power | Dummy mtkperf lib
 PRODUCT_PACKAGES += \
@@ -333,7 +337,11 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Vibrator
 PRODUCT_PACKAGES += \
