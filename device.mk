5c970cfc698b8df5efdf2231662077213626ae52
Woomymy <woomy@woomy.be>
2023-02-27 19:47:19 +0100

    LH7n: Install fstab.mt6789 into first_stage_ramdisk
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I6a0a98dcd8e5ff73dc8a934d1ea9fe43fd5107ca
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Init files
+PRODUCT_PACKAGES += \
+	fstab.mt6789_vendor_ramdisk
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayRozen \
