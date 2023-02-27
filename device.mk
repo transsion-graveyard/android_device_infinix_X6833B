88016e973b195facfabef47161ff8b3368c22965
Woomymy <woomy@woomy.be>
2023-02-27 15:31:40 +0100

    LH7n: Add partitions configuration
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I9e08db2236376b1132aa3bd8a7abf19aa70ba48c
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@ PRODUCT_PACKAGES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
