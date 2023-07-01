d5815e74034e3a7adbc1ce82a611294c8bd9a0f4
Christian Oder <myself5@carbonrom.org>
2023-07-01 17:20:17 +0700

    LH7n: Build disabled VBMeta image
    Change-Id: I48e3ff106a2e4e044ed39b86a94f3bc80b93f530
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,5 +107,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6789
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit the proprietary files
 include vendor/tecno/LH7n/BoardConfigVendor.mk
