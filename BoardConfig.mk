05d44fb766c4fb5edf5dcc274b579639c30b2a8f
bengris32 <bengris32@protonmail.ch>
2023-07-01 17:14:30 +0700

    LH7n: Define Bootloader and Platform flags
    * Use `rozen` for board name
    
    Change-Id: I8a4aad74ec90124cc35847c64d4db23f97af7ad7
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,14 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := rozen
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
+BOARD_HAS_MTK_HARDWARE := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
