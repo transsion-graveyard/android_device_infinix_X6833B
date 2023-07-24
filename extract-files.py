135741b6db6ef28930b4ce25532702f89fe513d1
Jiyong Park <jiyong@google.com>
2023-07-24 11:08:23 +0100

    X6833B: gnss: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Merged-In: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Change-Id: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,9 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service', 'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc': blob_fixup()
-        .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),   
+        .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
