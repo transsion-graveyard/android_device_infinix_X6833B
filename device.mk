145fd42c06768f2e7728fb7bf778036c19f333f6
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-11 09:06:56 +0700

    LH7n: Add keylayout file to map double tap event to wake
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.transsion
+    
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
 
 # Keymaster
 PRODUCT_COPY_FILES += \
