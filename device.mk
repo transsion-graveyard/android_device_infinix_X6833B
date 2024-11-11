fe0222ee1011adb81305ccda4be78abfcdf24b76
Woomymy <woomy@woomy.be>
2024-11-12 00:44:02 +0800

    X6833B: Enable USB audio accessory support
    Change-Id: Ib5c2fee5a774ebd1153d46f856f1208fe2227bbe
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -391,6 +391,8 @@ PRODUCT_PACKAGES += \
     libutils-v32
 
 # USB
+$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
