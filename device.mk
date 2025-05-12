13774d5961448e56150467a4c3eb422348270633
sashascurtu <sasha.scurtu@gmail.com>
2025-05-13 02:56:19 +0800

    X6833B: Build sensorservice 1.0 HAL
    Fixes:
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property ctl.interface_start to android.frameworks.sensorservice@1.0::ISensorManager/default: error code: 0x20
    
    Change-Id: If6fdef9dc39daaba9138fe3f8acf06cb6a0e78f8
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -422,6 +422,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor
