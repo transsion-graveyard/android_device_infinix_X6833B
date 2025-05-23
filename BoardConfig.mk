65192cbb0df5ccddbe08d12a3608022a5c220fc1
SamarV-121 <samar@samarv121.dev>
2025-05-23 23:37:08 +0800

    CK7n: Inherit common device FCM for mediatek HALs
    Change-Id: I6629f0d607a8875ee120a87a564f8366c1913458

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,9 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := 1
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(CONFIGS_PATH)/vintf/device_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+   $(CONFIGS_PATH)/vintf/device_framework_compatibility_matrix.xml \
+   hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(CONFIGS_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 
