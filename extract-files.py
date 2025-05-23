0673157b0545e0cb72c3661158763c6eb2afbccf
Giovanni Ricca <giovanniricca@proton.me>
2025-05-23 23:21:48 +0800

    LH7n: Migrate to py extract-utils
    Change-Id: I85fd64b0a90600cc0fa606a8746d49c731116925

---
+++ extract-files.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'LH7n',
+    'tecno',
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
