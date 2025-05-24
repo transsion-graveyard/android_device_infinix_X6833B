e22177403aace9aa485ce11df9c08b62aaa107ae
bengris32 <bengris32@protonmail.ch>
2025-05-24 12:39:56 +0800

    LH7n: Configure Pixel Thermal HAL
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Change-Id: I92fcc16ce01aae8b1b9307c38f4236dffb7f78b9
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -348,6 +348,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.transsion    
