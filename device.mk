0beb51bd672f267baa53c30ded0c5b18a0b8c843
baka-hokage <ayushbehera007@gmail.com>
2025-06-26 14:20:55 +0900

    X6833B: Include JamesDSP
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -256,6 +256,9 @@ PRODUCT_PACKAGES += \
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# JamesDSP
+$(call inherit-product-if-exists, vendor/JamesDSP/config.mk)
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.transsion
