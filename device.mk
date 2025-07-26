3c0c4e353669c30cb953004bcb3db51d40dddfd4
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2025-07-26 23:28:48 +0900

    LH7n: Configure Radio/IMS stack
    Refs: https://github.com/PQEnablers-Devices/android_device_xiaomi_ruby/commit/98ebd44a6a30656a121982cd659c88c86e0b3ba2
    [Shirayuki39]: We're gonna use the common mtk ims, no need to setup the IMS side of things completely
    
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -226,6 +226,11 @@ PRODUCT_COPY_FILES += \
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.6.vendor
+
 # Secure Element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
