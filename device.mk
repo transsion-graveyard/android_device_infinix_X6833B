8a90e32940cc6ccc6f06a425da19ff22ad592cfb
ChrisCatto <chriscatto@proton.me>
2025-07-26 23:34:27 +0900

    LH7n: Switch to common Mediatek IMS
    * Drop proprietary IMS blobs, ImsService.apk, duplicate overlays,
      props, permissions and patches.
    
    * Inherit the common Mediatek IMS makefile.
    
    Change-Id: Icfc413fd79baa635b9b5414b055bc71008bac823
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- device.mk
+++ device.mk
@@ -132,7 +132,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
     libhidltransport.vendor \
-    libhwbinder.vendor    
+    libhwbinder.vendor
+
+# IMS
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
