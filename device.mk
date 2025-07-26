481e36f2f57addf9ae25b7b955fe742fe8fc8cd3
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-26 21:48:08 +0900

    LH7n: Configure Keymaster stack
    Refs:
      https://github.com/nashc-dev/android_device_realme_nashc/tree/lineage-20
      https://github.com/PQEnablers-Devices/android_device_xiaomi_ruby/commit/8488358d934ee100e07e4a46ccc4cd36f1e13fa7
      https://github.com/Anything-at-25-00/android_device_tecno_CK7n_OLD/commit/59562c7f2d62715816b3dabd922937eb6e38e774
    
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Co-authored-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlayRozen \
     SystemUIResOverlayRozen
 
+# Keymaster
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
