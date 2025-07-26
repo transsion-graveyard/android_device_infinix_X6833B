96f49dd2f9c25198e2004f6a40b5b954f8e57811
Davide Garberi <dade.garberi@gmail.com>
2025-07-26 21:30:45 +0900

    X6833B: Add libhwbinder and libhidltransport vendor lib
    Change-Id: I7044b92fe012ace6005e1a3ae8534e9167da15ce

--- device.mk
+++ device.mk
@@ -79,6 +79,14 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor    
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Init files
 PRODUCT_PACKAGES += \
     factory_init.connectivity.common.rc \
