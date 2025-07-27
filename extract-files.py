2bcce91d08884f320424f6e7f98639146ad84765
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-27 09:40:19 +0900

    X6833B: Link PQ HAL to v32 libbinder and libhidlbase
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -16,6 +16,8 @@ from extract_utils.main import (
 
 blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service', 'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
+        .replace_needed('libbinder.so', 'libbinder-v32.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc': blob_fixup()
         .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
