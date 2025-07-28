4eb7c53b338621c2ff0634b5f0e4cb4ebbb468de
ChrisCatto <chriscatto08@gmail.com>
2025-07-28 23:27:56 +0900

    LH7n: Link mediatek camera provider against v32 libutils
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libbinder.so', 'libbinder-v32.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so')    
         .add_needed('libcamera_metadata_shim.so'),
 }  # fmt: skip
 
