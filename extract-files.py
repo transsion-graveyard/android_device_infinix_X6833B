aada9e7b02be15240f444a2dc83b10c07811c240
SamarV-121 <samarvispute121@pm.me>
2025-07-28 23:13:27 +0900

    LH7n: Link older libutils for camerahalserver
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- extract-files.py
+++ extract-files.py
@@ -64,6 +64,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
         .replace_needed('libbinder.so', 'libbinder-v32.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/bin/hw/mt6789/camerahalserver': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
