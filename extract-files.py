2083aeafa15f1c4bba4f5dbe2053bb441906dabb
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-28 22:54:48 +0900

    X6833B: Link vendor.mediatek.hardware.camera.isphal to vndk v32 libs
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     ('vendor/lib64/hw/audio.primary.mediatek.so'): blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
+    ('vendor/lib64/hw/mt6789/vendor.mediatek.hardware.camera.isphal@1.0-impl.so', 'vendor/lib64/hw/mt6789/vendor.mediatek.hardware.camera.isphal@1.1-impl.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
+        .replace_needed('libbinder.so', 'libbinder-v32.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
