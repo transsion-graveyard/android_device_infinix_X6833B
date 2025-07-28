053dc558a8efa18d8586ee4c8afd03aebcf14718
fjrXTR <fajarslebew31@gmail.com>
2025-07-28 23:44:37 +0900

    X6833B: Build Required Libs for QPR3
    Build Required libs for QPR3
    * ref - https://github.com/Evilmove/android_device_spartan/commit/55eb0b6bc717a005ee028de395d6e704db47a0a3 ObsidianMaximus/android_device_realme_sm8250-common@a0ed2f1
    
    Change-Id: I4696adea73edc5a8cfe530df6d89c5b244009722
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -66,13 +66,15 @@ PRODUCT_PACKAGES += \
     audio.usb.default:64
 
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor \
     libaudiofoundation.vendor:64 \
     libbluetooth_audio_session:64 \
     libalsautils:64 \
     libnbaio_mono:64 \
     libtinycompress:64 \
     libdynproc:64 \
-    libhapticgenerator:64
+    libhapticgenerator:64 \
+    libprocessgroup.vendor:64
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
@@ -106,7 +108,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libcamera_metadata_shim \
-    libdng_sdk.vendor        
+    libdng_sdk.vendor \
+    libexif.vendor \
+    libpiex \
+    libpng.vendor
     
 # Cgroup
 PRODUCT_COPY_FILES += \
@@ -120,6 +125,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek-mali
+
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     android.frameworks.sensorservice@1.0.vendor \
@@ -160,6 +166,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor \
     android.hardware.gnss-V1-ndk.vendor
 
+PRODUCT_PACKAGES += \
+    libcurl.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
@@ -170,6 +179,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     android.hidl.allocator@1.0.vendor \
+    libhidlmemory.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder.vendor
@@ -337,10 +347,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor
 
-# Power | Dummy mtkperf lib
+# Power
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
-    libmtkperf_client
+    libmtkperf_client \
+    libpower.vendor
 
 # Power configurations
 PRODUCT_COPY_FILES += \
@@ -363,6 +374,31 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio@1.6.vendor
+
+# Required for QPR3
+PRODUCT_PACKAGES += \
+    libexpat.vendor \
+    libunwindstack.vendor \
+    libchrome.vendor:64 \
+    libcurl.vendor \
+    libexif.vendor \
+    libdng_sdk.vendor \
+    liblz4.vendor \
+    libpiex \
+    libexpat.vendor \
+    libpng.vendor \
+    libion.vendor \
+    libui.vendor \
+    libmemunreachable.vendor \
+    libgatekeeper.vendor \
+    libjsoncpp.vendor \
+    libnetutils.vendor \
+    libdumpstateutil.vendor \
+    libruy.vendor \
+    libpcap.vendor \
+    libsqlite.vendor \
+    libutilscallstack.vendor \
+    libziparchive.vendor
 
 # Secure Element
 PRODUCT_PACKAGES += \
