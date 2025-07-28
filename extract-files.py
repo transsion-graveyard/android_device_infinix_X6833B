c00632880ca93c50de02f4589f8570b3aa4656e2
LuK1337 <priv.luk@gmail.com>
2025-07-28 23:25:52 +0900

    X6833B: Use libcamera_metadata_shim for unlocking cameras
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
         .replace_needed('libbinder.so', 'libbinder-v32.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
