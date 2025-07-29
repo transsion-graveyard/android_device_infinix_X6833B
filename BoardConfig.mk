1be29fad9d5c21969bbdca7ee23fca6afa80f56f
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-29 13:32:46 +0900

    LH7n: Prepare template system and vendor properties
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,9 @@
 
 DEVICE_PATH := device/tecno/LH7n
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Inherit the proprietary files
 include vendor/tecno/LH7n/BoardConfigVendor.mk
