f91408082105a4a05051db682c26deafdc5490f8
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-03 00:15:46 +0900

    X6833B: Cleanup trailing whitespaces in device.mk
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -499,7 +499,6 @@ PRODUCT_PACKAGES += \
 
 # USB
 $(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
-
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
@@ -508,7 +507,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    vndservicemanager \
    vndservice
-    
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     libwifi-hal-wrapper \
