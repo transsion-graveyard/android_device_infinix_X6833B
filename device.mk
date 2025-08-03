83aec36b68f73647a390cb6c057cfb21a3616d7a
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-03 11:12:24 +0900

    LH7n: Build required audio libs
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -71,12 +71,18 @@ PRODUCT_PACKAGES += \
     audioclient-types-aidl-cpp.vendor:64 \
     libaudioroute.vendor:64 \
     libaudiofoundation.vendor:64 \
+    libbundlewrapper:64 \
     libbluetooth_audio_session:64 \
+    libaudiopreprocessing:64 \
     libalsautils:64 \
+    libdownmix:64 \
+    libeffectproxy:64 \
     libnbaio_mono:64 \
     libtinycompress:64 \
     libdynproc:64 \
     libhapticgenerator:64 \
+    libldnhncr:64 \
+    libreverbwrapper:64 \
     libprocessgroup.vendor:64
 
 PRODUCT_PACKAGES += \
