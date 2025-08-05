e7cd70273f1fdd9b0f52d1e2ae7b5b7170a02681
ChrisCatto <chriscatto@proton.me>
2025-08-05 23:57:22 +0900

    X6833B: Patch `mtkfusionrild` to load v32 `libutils`
    * Fixes MTK RIL crashes after Android 16 release
    
    Change-Id: If4356c3e0ff8f0832bec772e70a12a311b55e582
    Signed-off-by: ChrisCatto <chriscatto@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -99,6 +99,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/bin/hw/mtkfusionrild': blob_fixup()
+        .add_needed('libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
