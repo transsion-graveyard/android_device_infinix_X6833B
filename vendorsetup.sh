a5af8ad7473c2d3bee33375d134653e91375ed7e
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-14 23:34:06 +0900

    LH7n: vendorsetup: Update Aperture MediaTek HFPS mode patch commit hash
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@
 
 echo "- Applying Aperture Mediatek HFPS Mode Patch"
 cd packages/apps/Aperture
-curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/a4c34aa57ed56de60f29349a1e6d20cf8160ca15.patch | git am
+curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/9509277efc852ad8bdcce204e0d9cfe104b6d190.patch | git am
 cd ../../../
