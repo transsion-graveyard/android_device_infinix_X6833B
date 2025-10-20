7fe712609e412a1d03494f107a8dd1622a68d30d
Vaisakh Murali <vaisakhmurali@gmail.com>
2025-10-20 17:17:17 +0900

    X6833B: init: drop init.aee.rc
    [Shirayuki39]: Useless, we don't even have these services as we're not shipping them
    [Apichat1234inwza007]: Also dropping from system_ext
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -234,7 +234,6 @@ PRODUCT_PACKAGES += \
     fstab.mt6789 \
     fstab.mt6789.vendor_ramdisk \
     init_connectivity.rc \
-    init.aee.rc \
     init.connectivity.common.rc \
     init.connectivity.rc \
     init.insmod.sh \
