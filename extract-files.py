55a9a4419f33d710e6f5e56dcd22a5b308cd1896
bengris32 <bengris32@protonmail.ch>
2026-01-10 14:04:00 +0800

    X6833B: Link main Audio HW module against V33 libstagefright_foundation
    Change-Id: Id2dfd90a671846fb0108503d642195eaa4a25f12
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     ('vendor/lib64/hw/audio.primary.mediatek.so'): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v31.so')
         .replace_needed('libutils.so', 'libutils-v32.so')
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
