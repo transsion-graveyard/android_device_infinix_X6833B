0cc4a9bd57dfbb933de2792016d8166760812956
Shirayuki39 <lorddemecrius83@proton.me>
2026-02-19 19:19:59 +0800

    X6833B: Ship proprietary firmware
    * To make the install process simpler
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -114,6 +114,7 @@ module = ExtractUtilsModule(
     'infinix',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
