c91c28d802e0ea9d1168b677bc6fbce952703d40
Apichat1234inwza007 <discom610@gmail.com>
2026-04-11 01:55:43 +0700

    Revert "X6833B: Dolby Atmos : Activate"
    This reverts commit 5a7d9483e2af5323f8948356aed4fcfd010b24b6.
    
    Signed-off-by: Apichat1234inwza007 <discom610@gmail.com>

--- device.mk
+++ device.mk
@@ -163,9 +163,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ANGLE
 
-# Dolby
-$(call inherit-product, vendor/sony/dolby/setup.mk)
-
 # DRM
 PRODUCT_PACKAGES += \
     com.android.hardware.drm.clearkey
