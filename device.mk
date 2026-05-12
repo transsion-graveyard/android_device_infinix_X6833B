12c851c6eb7ad98f179861a144e929f9a0e93a00
Shirayuki39 <lorddemecrius83@proton.me>
2026-05-12 12:07:46 +0800

    X6833B: Update usb soong config to bool type
    * Extension to 6b7e037
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -512,7 +512,7 @@ PRODUCT_PACKAGES += \
     libutils-v32:64
 
 # USB
-$(call soong_config_set,android_hardware_mediatek_usb,audio_accessory_supported,true)
+$(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_supported,true)
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
     android.hardware.usb.gadget-service.mediatek
